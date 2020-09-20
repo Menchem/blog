8b41e7f4f3903ef903e0ccfd79f00b025659732d
zhou <284939796@qq.com>
2020-09-20 21:52:51 +0800

    blog

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -2,13 +2,16 @@
 <div class="category">
   <HeaderBox></HeaderBox>
   <div class="cate-box">
-    <div class="catelist" v-for="item in listarr">
+    <transition-group tag="div" name="myZoom">
+    <div class="catelist" v-for="(item,index) in listarr" :key="'list'+index">
       <div class="title"><router-link :to="'/show/'+item.id">{{item.title}}</router-link></div>
       <div class="time">{{item.create_time}}</div>
       <div class="description">{{item.description}}...</div>
       <div class="link"><router-link :to="'/show/'+item.id">查看</router-link></div>
     </div>
+    </transition-group>
   </div>
+  <div class="content-loading" v-if="loadShow" v-html="categoryHtml"></div>
   <Myfooter></Myfooter>
 </div>
 </template>
@@ -21,7 +24,12 @@ export default {
   name: 'Category',
   data(){
     return {
-      listarr: []
+      page: 1,
+      allArr: [],
+      listarr: [],
+      loadShow: true,
+      showMore: false,
+      categoryHtml: "内容正在加载，请稍后...",
     }
   },
   components:{
@@ -33,8 +41,53 @@ export default {
       methods: 'get',
       url: 'http://admin.zhoumengcheng.cn/showall',
     }).then((res)=>{
-      this.listarr = res.data.content;
+      this.allArr = res.data.content;
+      this.listarr = this.allArr.slice(0,6);
     });
+    this.$nextTick(()=>{
+      window.addEventListener('scroll',this.getList)
+    });
+  },
+  methods:{
+    getList(){
+      let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+      let elHeight = document.documentElement.clientHeight || document.body.clientHeight;
+      let catBox = document.querySelector(".cate-box");
+      let catHeihgt = catBox.clientHeight;
+      let toButtom = catHeihgt + 100 - elHeight - scrollTop ;
+      if(toButtom < 20){
+
+        setTimeout(()=>{
+
+          let moreNum = this.allArr.length % (this.page*6);
+          let tmpArr = [];
+          if(this.showMore){
+            this.categoryHtml = "我可是有底线的-_-!";
+            return ;
+          }
+          if(moreNum < 6){
+            tmpArr = this.allArr.slice(this.page*6,this.page*6 + moreNum);
+            for(let i = 0; i < tmpArr.length; i++){
+                this.listarr.push(tmpArr[i]);
+            }
+            this.showMore = true;
+          }else{
+            tmpArr = this.allArr.slice(this.page*6,this.page*6 + 6);
+            for(let i = 0; i < tmpArr.length; i++){
+              this.listarr.push(tmpArr[i]);
+            }
+            this.page++;
+          }
+
+
+        },500);
+
+      }
+
+    }
+  },
+  destroyed() {
+    window.removeEventListener('scroll',this.getList);
   }
 }
 </script>
@@ -83,6 +136,19 @@ export default {
         color: #fff;
       }
     }
+  }
+}
+.myZoom-enter-active{
+  animation: myZoom .5s linear both;
+}
+@keyframes myZoom{
+  0%{
+    opacity: 0;
+    transform: scale(0);
+  }
+  100%{
+    opacity: 1;
+    transform: scale(1);
   }
 }
 </style>
